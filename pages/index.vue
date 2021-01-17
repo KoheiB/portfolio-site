09d923408e54b4b80a99708ac06bb157092cdf65
Kohei Terashita <kou0704terasita@gmail.com>
2021-01-18 00:27:40 +0900

    add section

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,14 @@
   div
     header
     main
-      h1.red hello
+      div#about.section
+        h1 About
+      div#works.section
+        h1 Works
+      div#skill.section
+        h1 Skill
+      div#contact.section
+        h1 Contact
     footer
 </template>
 
@@ -11,9 +18,26 @@ export default {}
 </script>
 
 <style>
-.red {
-  color: red;
+h1 {
+  font-size: 3.56rem;
 }
+.section {
+  width: 100%;
+  min-height: 100vh;
+}
+#about {
+  background-color: pink;
+}
+#works {
+  background-color: brown;
+}
+#skill {
+  background-color: purple;
+}
+#contact {
+  background-color: gray;
+}
+
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
